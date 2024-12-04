8cf9a8688d4991beadfa3b41e3605d445951ba3d
rpanca15 <panca151204@gmail.com>
2024-12-04 09:07:14 +0700

    first commit

---
+++ app/Rules/Captcha.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use ReCaptcha\ReCaptcha;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Captcha implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        
+    }
+
+    public function passes($attribute, $value)
+    {
+        $recaptcha = new ReCaptcha(env('CAPTCHA_SECRET'));
+        $response = $recaptcha->verify($value, $_SERVER['REMOTE_ADDR']);
+        return $response->isSuccess();
+    }
+}
